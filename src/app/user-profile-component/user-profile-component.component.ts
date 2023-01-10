223a783b9a5082316580fbae89d12a7a0a0d8bed
LordWinNgoran <cyrille4011@gmail.com>
2023-01-10 09:06:50 +0100

    Fix ngOninit

--- src/app/user-profile-component/user-profile-component.component.ts
+++ src/app/user-profile-component/user-profile-component.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 class User {
   first_name!:string;
@@ -10,15 +10,21 @@ class User {
   templateUrl: './user-profile-component.component.html',
   styleUrls: ['./user-profile-component.component.css']
 })
-export class UserProfileComponentComponent {
+export class UserProfileComponentComponent implements OnInit {
 
 user:User= {
   first_name :"Lordwin james",
   last_name:"N'goran",
   profilePictureUrl:"https://media.licdn.com/dms/image/D4E03AQEBOmPOvff9iQ/profile-displayphoto-shrink_200_200/0/1671783099289?e=1678924800&v=beta&t=bj6H7DAdyK8jV7NYHq4TXDbt6zmR24mocXtSV2jpV44",
 }
+constructor(){
+}
+
+ngOnInit(): void {
+}
 
 getFullName(){
-    return this.user.first_name+' '+this.user.last_name
+  return this.user.first_name+' '+this.user.last_name
 }
+
 }
