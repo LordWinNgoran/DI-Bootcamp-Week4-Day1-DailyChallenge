74cd346b7c01f699a8b36fe5b6beb75ffb64a511
LordWinNgoran <cyrille4011@gmail.com>
2023-01-10 08:24:51 +0100

    DI-Bootcamp Week4 Day1 Daily Challenge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserProfileComponentComponent } from './user-profile-component/user-profile-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponentComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/user-profile-component/user-profile-component.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+class User {
+  first_name!:string;
+  last_name!:string;
+  profilePictureUrl!:string
+}
+@Component({
+  selector: 'app-user-profile-component',
+  templateUrl: './user-profile-component.component.html',
+  styleUrls: ['./user-profile-component.component.css']
+})
+export class UserProfileComponentComponent {
+
+user:User= {
+  first_name :"Lordwin james",
+  last_name:"N'goran",
+  profilePictureUrl:"https://media.licdn.com/dms/image/D4E03AQEBOmPOvff9iQ/profile-displayphoto-shrink_200_200/0/1671783099289?e=1678924800&v=beta&t=bj6H7DAdyK8jV7NYHq4TXDbt6zmR24mocXtSV2jpV44",
+}
+
+getFullName(){
+    return this.user.first_name+' '+this.user.last_name
+}
+}
